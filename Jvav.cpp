852490504830272848dee4b04dbb24dc5338ca7d
hongshige <2402210783@qq.com>
2020-04-16 00:23:13 +0800

    Update Jvav.cpp

--- Jvav.cpp
+++ Jvav.cpp
@@ -1,5 +1,5 @@
-﻿/*
-    Jvav Programmer Ver.02w05c Snapshot
+/*
+    Jvav Programmer Ver.02w05d Snapshot
     By 30266
     Compilation time 2020-04-15 20:32:23
 */
@@ -13,12 +13,12 @@ int main()
         cout << "Getting a list of files...\n";
         Sleep(150);
         cout << "Detecting file integrity...\n";
-        Sleep(2000);
+        Sleep(200);
         cout << "Error! There are 15 files that go wrong! Re-downloading.\n";
         cout << "Downloading file (1/15)...\nCL.command.1.tlog\n";
         Sleep(120);
         cout << "Downloading file (2/15)...\nCL.read.1.tlog\n";
-        Sleep(9110);
+        Sleep(911);
         cout << "Downloading file (3/15)...\nCL.write.1.tlog\n";
         Sleep(130);
         cout << "Downloading file (4/15)...\nJvav.lastbuildstate\n";
@@ -26,43 +26,43 @@ int main()
         cout << "Downloading file (5/15)...\nlink.command.1.tlog\n";
         Sleep(230);
         cout << "Downloading file (6/15)...\nlink.read.1.tlog\n";
-        Sleep(2930);
+        Sleep(290);
         cout << "Downloading file (7/15)...\nlink.write.1.tlog\n";
         Sleep(150);
         cout << "Downloading file (8/15)...\nJvav.ilk\n";
-        Sleep(7820);
+        Sleep(720);
         cout << "Downloading file (9/15)...\nJvav.txt\n";
         Sleep(130);
         cout << "Downloading file (10/15)...\nJvav.zip\n";
-        Sleep(5910);
+        Sleep(910);
         cout << "Downloading file (11/15)...\nJvav.pdb\n";
-        Sleep(6110);
+        Sleep(110);
         cout << "Downloading file (12/15)...\nvc142.idb\n";
-        Sleep(1030);
+        Sleep(300);
         cout << "Downloading file (13/15)...\nvc142.pdb\n";
-        Sleep(2500);
+        Sleep(250);
         cout << "Downloading file (14/15)...\nonline upgrades.service\n";
-        Sleep(1500);
+        Sleep(150);
         cout << "Downloading file (15/15)...\nlanguage pack.pack\n";
-        Sleep(1200);
+        Sleep(120);
         cout << "Verifying downloaded files...\n";
         Sleep(120);
         cout << "Registered and profile...\n";
-        Sleep(1500);
+        Sleep(150);
         cout << "Loading file...\n";
-        Sleep(5000);
+        Sleep(500);
         system("cls");
     }
     /*en_main*/
     string command, type, inputcharacter;
     cout << "--------------------------------------------\n";
-    cout << "| Jvav Programm         Ver.02w05cSnapshot |\n";
+    cout << "| Jvav Programm         Ver.02w05dSnapshot |\n";
     cout << "| By Dr.ZhangHaoYang      Programmer 30266 |\n";
     cout << "| Enter'help'to get help!                  |\n";
     cout << "| Chinese Simplified is now supported!     |\n";
     cout << "--------------------------------------------\n";
 main:
-    cout << "Jvav>";
+    cout << ">";
     cin >> command;
     if (command == "help") {
         cout << "----Jvav help------Page(1/1)---\n";
@@ -82,24 +82,24 @@ main:
         }
     }
     else if (command == "output") {
-        cout << "Jvav>output>";
+        cout << ">>";
         cin >> type;
         cout << type << endl;
         goto main;
     }
     else if (command == "info") {
-        cout << "Jvav Programm Ver.02w05c\nIt's just a joke, but we still make it, and the joke was first brought by Zhang Haoyang.\nThe person writing the program is 30266.\nThis version fixes translation errors and adds the info feature.\nCompilation was completed on April 15, 2020, 20:32.\n";
+        cout << "Jvav Programm Ver.02w05d\nIt's just a joke, but we still make it, and the joke was first brought by Zhang Haoyang.\nThe person writing the program is 30266.\nThis version fixes translation errors and adds the info feature.\nCompilation was completed on April 15, 2020, 20:32.\n";
         goto main;
     }
     else if (command == "input") {
-        cout << "Jvav>input>";
+        cout << ">i>";
         cin >> inputcharacter;
         goto main;
     }
     else if (command == "upgrade") {
-        cout << "Jvav>upgrade>Detecting updates online...\n";
+        cout << ">>U-Detecting updates online...\n";
         Sleep(5000);
-        cout << "Jvav>upgrade>Error! The update failed! Because the version list cannot be obtained on the authentication server!\n";
+        cout << ">>U-Error! The update failed! Because the version list cannot be obtained on the authentication server!\n";
         goto main;
     }
     else if (command == "language") {
@@ -114,7 +114,7 @@ main:
             goto ch;
         }
         else {
-            cout << "Unknown language.\n";
+            cout << "Unknown language\n";
             goto main;
         }
     }
@@ -126,14 +126,14 @@ main:
     /*cn_main*/
 ch:
     cout << "--------------------------------------------\n";
-    cout << "| Jvav编译器            版本号：02f05c快照 |\n";
+    cout << "| Jvav命令行工具        版本号：02f05d快照 |\n";
     cout << "| 作者：张浩洋大师           编译员：30266 |\n";
     cout << "| 通过输入'帮助'来获得帮助                 |\n";
     cout << "| 现已支持简体中文！                       |\n";
     cout << "--------------------------------------------\n";
     string 命令, 类型, 输入字符串;
 cn_main:
-    cout << "Jvav>";
+    cout << ">";
     cin >> 命令;
     if (命令 == "帮助") {
         cout << "----Jvav帮助---第(1/1)页----\n";
@@ -153,24 +153,24 @@ cn_main:
         }
     }
     else if (命令 == "输出") {
-        cout << "Jvav>输出>";
+        cout << ">>";
         cin >> 类型;
         cout << 类型 << endl;
         goto cn_main;
     }
     else if (命令 == "输入") {
-        cout << "Jvav>输入>";
+        cout << ">>";
         cin >> 输入字符串;
         goto cn_main;
     }
     else if (命令 == "关于") {
-        cout << "Jvav编程器，版本为02f05c快照。\n这只是一个梗，但是我们依然把它变成了现实。最早是张浩洋提出的这个梗。\n该版本修复了翻译错误问题并添加了关于功能。\n该程序编写者为30266。\n编译完成于2020年04月15日，20时32分。\n";
+        cout << "Jvav编程器，版本为02f05d快照。\n这只是一个梗，但是我们依然把它变成了现实。最早是张浩洋提出的这个梗。\n该版本修复了翻译错误问题并添加了关于功能。\n该程序编写者为30266。\n编译完成于2020年04月15日，20时32分。\n";
         goto cn_main;
     }
     else if (命令 == "更新") {
-        cout << "Jvav>更新>正在准备更新...\n";
+        cout << ">>U-正在准备更新...\n";
         Sleep(5000);
-        cout << "Jvav>更新>错误！无法从认证服务器获取版本列表！\n";
+        cout << ">>U-错误！无法从认证服务器获取版本列表！\n";
         goto cn_main;
     }
     else if (命令 == "语言") {
@@ -183,7 +183,7 @@ cn_main:
         else if (语言 == "English") {
             system("cls");
             cout << "--------------------------------------------\n";
-            cout << "| Jvav Programm         Ver.02w05cSnapshot |\n";
+            cout << "| Jvav Programm         Ver.02w05dSnapshot |\n";
             cout << "| By Dr.ZhangHaoYang      Programmer 30266 |\n";
             cout << "| Enter'help'to get help!                  |\n";
             cout << "| Chinese Simplified is now supported!     |\n";
@@ -191,12 +191,12 @@ cn_main:
             goto main;
         }
         else {
-            cout << "未知的语言。\n";
+            cout << "未知语言\n";
             goto cn_main;
         }
     }
     else {
-        cout << "未知的命令！\n";
+        cout << "未知命令\n";
         goto cn_main;
     }
 }
